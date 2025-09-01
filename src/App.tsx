6625b0220b49de5b3476efec38476513307baf00
Joanna M <34979359+Johanka2014@users.noreply.github.com>
2025-09-01 13:27:47 +0200

    Update App.tsx
    Updated links to conversational English

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import BusinessEnglish from "./pages/BusinessEnglish";
+import ConversationalEnglish from "./pages/Conversation";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -18,6 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/business-english" element={<BusinessEnglish />} />
+          <Route path="/conversational-english" element={<ConversationalEnglish />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
