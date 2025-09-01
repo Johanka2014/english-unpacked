b4c805289f800a468c42925334c70dcb72da4be4
Joanna M <34979359+Johanka2014@users.noreply.github.com>
2025-09-01 14:11:01 +0200

    Update App.tsx
    added links to the exam prep page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import BusinessEnglish from "./pages/BusinessEnglish";
 import ConversationalEnglish from "./pages/Conversation";
+import ExamPreparation from "./pages/ExamPreparation";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -20,6 +21,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/business-english" element={<BusinessEnglish />} />
           <Route path="/conversational-english" element={<ConversationalEnglish />} />
+          <Route path="/exam-preparation" element={<ExamPreparation />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
